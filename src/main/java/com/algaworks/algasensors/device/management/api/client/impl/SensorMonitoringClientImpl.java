13cba91bedd944dde47c2e36f71a8f295f47815c
jucami90 <jucami90@gmail.com>
2025-11-22 12:04:54 -0500

    add  creando metodo get para sensores detalles

--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,24 +1,21 @@
 package com.algaworks.algasensors.device.management.api.client.impl;
 
+import com.algaworks.algasensors.device.management.api.client.RestClientFactory;
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
-import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
+import com.algaworks.algasensors.device.management.api.model.SensorMOnitoringOutput;
 import io.hypersistence.tsid.TSID;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     private final RestClient restClient;
 
-    public SensorMonitoringClientImpl(RestClient.Builder restClient) {
-        this.restClient = RestClient.builder()
-                .baseUrl("http://localhost:8082")
-                .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
-                    throw new SensorMonitoringClientBadGatewayException();
-                }))
-                .build();
+    public SensorMonitoringClientImpl(RestClientFactory factory) {
+        this.restClient = factory.temperatureMonitoringRestCLient();
     }
 
 
@@ -35,5 +32,13 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
                 .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
                 .retrieve().toBodilessEntity();
 
+    }
+
+    @Override
+    public SensorMOnitoringOutput getDetail(TSID sensorId) {
+        return restClient.get()
+                .uri("/api/sensors/{sensorId}/monitoring", sensorId)
+                .retrieve()
+                .body(SensorMOnitoringOutput.class);
     }
 }
